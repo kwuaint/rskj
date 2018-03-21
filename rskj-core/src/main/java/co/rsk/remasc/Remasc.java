6956a749ab1893d21885e8896a4a0b69cf53ad9b
Matias Marquez <sir.incognita@gmail.com>
2018-03-21 20:14:50 -0300

    vm refactor

--- rskj-core/src/main/java/co/rsk/remasc/Remasc.java
+++ rskj-core/src/main/java/co/rsk/remasc/Remasc.java
@@ -126,7 +126,19 @@ public class Remasc {
         feesPayer.payMiningFees(processingBlockHeader.getHash().getBytes(), payToRskLabs, remascConstants.getRskLabsAddress(), logs);
         fullBlockReward = fullBlockReward.subtract(payToRskLabs);
 
-        RemascFederationProvider federationProvider = new RemascFederationProvider(config, repository, processingBlock);
+        // TODO to improve
+        // and we need a RepositoryTrack to feed RemascFederationProvider
+        // because it supports the update of bytes (notably, RepositoryImpl don't)
+        // the update of bytes is needed, because BridgeSupport creation could alter
+        // the storage when getChainHead is null (specially in production)
+        BridgeSupport bridgeSupport = new BridgeSupport(
+                config,
+                repository,
+                PrecompiledContracts.BRIDGE_ADDR,
+                processingBlock
+        );
+
+        RemascFederationProvider federationProvider = new RemascFederationProvider(bridgeSupport);
 
         Coin payToFederation = fullBlockReward.divide(BigInteger.valueOf(remascConstants.getFederationDivisor()));
 
